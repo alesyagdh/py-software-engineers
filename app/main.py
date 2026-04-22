c6f2cc051aff2aa7781c79d420be3760cec82761
Alesya <alesyagdh@gmail.com>
2026-04-23 00:32:17 +0300

    Solution #3

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(self, name)
+        super().__init__(name)
         self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_web_application(self) -> None:
